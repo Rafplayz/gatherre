75092a11ecd8b6dabf26f4b0df7f86176efa07e1
Rafplayz <60671867+Rafplayz@users.noreply.github.com>
2022-02-16 14:07:20 -0500

    player interface

---
+++ ts/Player.ts
@@ -0,0 +1,4 @@
+interface Player {
+    berries: number;
+    
+}
--- ts/game.ts
+++ ts/game.ts
@@ -1,11 +1,28 @@
 const updateInterval = setInterval(Update,100)
-let berries: number
-// DEBUG: replace below
-berries = 1
-
+const autosaveInterval = setInterval(save,60000)
+let player: Player;
+function getInitialPlayer() {
+    return {
+        berries: 0,
+    }
+}
+function save() {
+    window.localStorage.setItem('player',JSON.stringify(player))
+    return JSON.stringify(player)
+}
+function load() {
+    const localStoredPlayer = localStorage.getItem('player')
+    if(localStoredPlayer == undefined){player = getInitialPlayer();return}
+    try {
+    player = JSON.parse(localStoredPlayer)
+    }
+    catch(err) {
+        console.error(err) // TODO: add an error popup
+    }
+}
 function gatherItems(): void{
     console.log("gatherItems received")
-    berries++
+    player.berries++
 }
 const gatherButton = new TimedButton(
     "Gather (timed)",
@@ -16,10 +33,11 @@ const gatherButton = new TimedButton(
 )
 gatherButton.assign("gatherButton")
 function Update(): void {
-    updateElement("main",`Berries: ${berries}`)
+    updateElement("main",`Berries: ${player.berries}`)
 }
-function updateElement(elementID: string,text: string) {
+function updateElement(elementID: string,text: string): void {
     let elem = document.getElementById(elementID)
     if(elem == null) throw new Error("elementID must be a valid ID")
     elem.innerText = text
 }
+load()

31e0be847a95a7e1077f38014eb8ddc96962e70e
Raf <60671867+Rafplayz@users.noreply.github.com>
2022-03-25 17:11:00 -0400

    the randomization (double tap if you'd skrunkly)

--- ts/game.ts
+++ ts/game.ts
@@ -15,16 +15,36 @@ gm.initTimeouts(
 
 
 
-function gatherItems(): void{
-    const rand = Math.random()
-    
-    if(rand >= 0.5) {
-        PLAYER.berries = PLAYER.berries + 2n
+function gatherItems(): void {
+
+    // the idea is to make separate "loot tables", kind of like how in minecraft you can get emeralds and bones,
+    // or emeralds and bone meals, or diamonds and bones, etc.
+    const randpool1 = Math.randInt(1,5)
+    const randpool2 = Math.randInt(1,5)
+
+    switch(randpool1) {
+        case 1:
+        case 2:
+            PLAYER.berries++
+            break
+        case 3:
+            break
+        case 4:
+            PLAYER.berries = PLAYER.berries + 2n
+            break
+        case 5:
+            PLAYER.berries = PLAYER.berries + 3n
+            break
     }
-    else {
-        PLAYER.berries++
+    switch (randpool2) {
+        // make sticks much rarer than berries
+        case 5:
+            PLAYER.sticks++
+            break
+        default:
+            console.log("nothing")
+            break
     }
-    console.log("gatherItems received")
 }
 
 

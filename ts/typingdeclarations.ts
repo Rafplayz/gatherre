80f882613ee3c43eab66e526d34d677079c44084
Raf <60671867+Rafplayz@users.noreply.github.com>
2022-03-25 16:46:48 -0400

    the renaming

--- ts/typingdeclarations.ts
+++ ts/typingdeclarations.ts
@@ -4,6 +4,16 @@ interface Math {
 interface BigInt {
     toJSON():string
 }
+interface Player {
+    VERSION: {
+        number: string
+        isBeta: boolean
+        isPrerelease: boolean
+        name?: string
+    }
+    berries: bigint;
+    sticks: bigint;
+}
 Math.randInt = (min: number, max: number): number => {
     min = Math.ceil(min);
     max = Math.floor(max);
